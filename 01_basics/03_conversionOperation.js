30c567e5c99a02c86830304bfc3f800e73a4205d
Atamjivanjot Kaur <106860632+Atamjivanjot@users.noreply.github.com>
2023-10-21 04:49:20 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,58 @@
-let score  = 33;
+// let score  = 33;
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-//"33" => 33
-// "33abc" => NaN
-// true => 1 ; false=> 1
+// //"33" => 33
+// // "33abc" => NaN
+// // true => 1 ; false=> 1
 
-let isLooggedIn = 1
-let booleanIsLoggedIn = Boolean(isLooggedIn);
-console.log(booleanIsLoggedIn);
+// let isLooggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLooggedIn);
+// console.log(booleanIsLoggedIn);
 
-//1 => true ;  0 => false
-// "" => false
-// "hitesh" => true
+// //1 => true ;  0 => false
+// // "" => false
+// // "hitesh" => true
 
 
-let a = 33;
-let stringNumber = String(a);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let a = 33;
+// let stringNumber = String(a);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/**************OPERATION******************/
+
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+
+// console.table([2+2, 2/2, 2%2, 2-2]);
+
+let str1 = "hello"
+let str2 =" hitesh"
+
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log("1" + 2 +2);
+// console.log(1+ 1 + "2");
+
+// console.log(true); //1
+// console.log(+""); //0
+
+
+let num1, num2 ,num3 
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
